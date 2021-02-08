36d07cce2095f160a0e02c5f33486a045db2a1b0
Julen Barrientos <julenbarrientos@gmail.com>
2021-02-08 12:25:28 +0100

    Add project files.

---
+++ SDTransferApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using SDTransferApp.Services;
+using SDTransferApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SDTransferApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ SDTransferApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using SDTransferApp.Models;
+using SDTransferApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SDTransferApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
